ec346429c849e195cedf0533b2893436b22ba6b2
aLeX <96099691+alexhacks007@users.noreply.github.com>
2024-10-29 11:48:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <BrowserRouter>
     <Navbar/>
     <Routes>
-      <Route path='' element={<Home/>} />
+      <Route path='/' element={<Home/>} />
       {/* <Route path='login' element={<Login/>} /> */}
 
     </Routes>
